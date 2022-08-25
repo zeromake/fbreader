c68bdd485414d423c71f97f7f96f909dce104b55
zeromake <a390720046@gmail.com>
2022-08-25 21:32:03 +0800

    feat: mac osx build fix

--- makefiles/arch/macosx.mk
+++ makefiles/arch/macosx.mk
@@ -15,29 +15,26 @@ APPIMAGEDIR_MACRO = $(SHAREDIR_MACRO)/icons
 ZLSHARED = no
 
 TOOLSPATH = /Developer/usr/bin
-CC = $(TOOLSPATH)/gcc-4.2
-AR = $(TOOLSPATH)/ar rsu
-LD = $(TOOLSPATH)/g++-4.2
-
-MACOS_VERSION = 10.5
+OSXSDK = /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
+CC = clang
+AR = ar rsu
+LD = clang
 
 #ARCH_FLAGS = -arch x86_64 -arch i386 -arch ppc7400 -arch ppc64
-ARCH_FLAGS = -arch i386 -arch ppc7400
+ARCH_FLAGS = -arch x86_64
 CFLAGS_NOARCH = \
 	-fmessage-length=0 -pipe -fpascal-strings -fasm-blocks \
 	-mdynamic-no-pic -W -Wall \
-	-isysroot /Developer/SDKs/MacOSX$(MACOS_VERSION).sdk \
+	-isysroot $(OSXSDK) \
 	-fvisibility=hidden -fvisibility-inlines-hidden \
-	-mmacosx-version-min=$(MACOS_VERSION) \
-	-gdwarf-2
+	-gdwarf-2 -g
 CFLAGS = $(ARCH_FLAGS) $(CFLAGS_NOARCH)
 LDFLAGS = $(ARCH_FLAGS) \
-	-isysroot /Developer/SDKs/MacOSX$(MACOS_VERSION).sdk \
-	-mmacosx-version-min=$(MACOS_VERSION)
-EXTERNAL_INCLUDE = -I/usr/local/include
+	-isysroot $(OSXSDK)
+EXTERNAL_INCLUDE = -I /usr/local/include -I $(OSXSDK)/usr/include
 EXTERNAL_LIBS = -liconv
 
-UILIBS = -framework Cocoa $(ROOTDIR)/zlibrary/ui/src/cocoa/application/CocoaWindow.o $(ROOTDIR)/zlibrary/ui/src/cocoa/library/ZLCocoaAppDelegate.o
+UILIBS = -framework Appkit $(ROOTDIR)/zlibrary/ui/src/cocoa/application/CocoaWindow.o $(ROOTDIR)/zlibrary/ui/src/cocoa/library/ZLCocoaAppDelegate.o
 
 RM = rm -rvf
 RM_QUIET = rm -rf
--- makefiles/config.mk
+++ makefiles/config.mk
@@ -38,16 +38,16 @@ ifeq "$(TARGET_STATUS)" "profile"
 	LDFLAGS += -pg
 endif
 
-ZINCLUDE = -I $(ROOTDIR)/zlibrary/core/include -I $(ROOTDIR)/zlibrary/text/include
+ZINCLUDE = -I $(ROOTDIR)/zlibrary/core/include -I $(ROOTDIR)/zlibrary/text/include $(EXTERNAL_ZINCLUDE)
 
 ZLSHARED ?= yes
 
 ifeq "$(ZLSHARED)" "yes"
-  CORE_LIBS = -lm -L$(ROOTDIR)/zlibrary/core -lzlcore -ldl
-	TEXT_LIBS = -lzltext
+  CORE_LIBS = -lm -L$(ROOTDIR)/zlibrary/core -lzlcore -ldl $(EXTERNAL_ZLIBS)
+  TEXT_LIBS = -lzltext $(EXTERNAL_ZLIBS)
 else
-  CORE_LIBS = -lm -L$(ROOTDIR)/zlibrary/ui -L$(ROOTDIR)/zlibrary/core -lzlcore -lzlui-$(UI_TYPE) -lzlcore $(UILIBS) $(XML_LIBS) $(ARCHIVER_LIBS) $(NETWORK_LIBS)
-	TEXT_LIBS = -lzltext $(EXTERNAL_LIBS) -llinebreak -lfribidi
+  CORE_LIBS = -lm -L$(ROOTDIR)/zlibrary/ui -L$(ROOTDIR)/zlibrary/core -lzlcore -lzlui-$(UI_TYPE) -lzlcore $(UILIBS) $(XML_LIBS) $(ARCHIVER_LIBS) $(NETWORK_LIBS) $(EXTERNAL_ZLIBS) -lstdc++
+  TEXT_LIBS = -lzltext $(EXTERNAL_LIBS) -lunibreak -lfribidi -lstdc++
 endif
 
 ifneq "$(BUILD_RESOURCE_OBJECT)" "yes"
