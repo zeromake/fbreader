d199737a69b108e23c607c317bee15c044168565
zeromake <a390720046@gmail.com>
2022-10-12 18:50:02 +0800

    fix: action workflows

--- build-3rd.ps1
+++ build-3rd.ps1
@@ -1,6 +1,8 @@
+Try{
 Remove-Item -Force -Recurse .\3rd\include
 Remove-Item -Force -Recurse .\3rd\lib
 Remove-Item -Force -Recurse .\3rd\build
+}Catch{}
 xmake f -c --file=.\3rd\xmake.lua
 xmake lua .\3rd\fetch.lua
 xmake build --file=.\3rd\xmake.lua
